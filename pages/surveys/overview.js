5be41b7c0007c75854452f42c4061e611bd53c26
allu <aleksijaakkojuhani@gmail.com>
2020-11-24 10:55:59 +0200

    survey fetch logic

--- pages/surveys/overview.js
+++ pages/surveys/overview.js
@@ -2,37 +2,44 @@ import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import { useEffect, useRef } from 'react';
 import jwt from 'next-auth/jwt';
+import axios from 'axios';
 
-export default function SurveyOverview({ token }) {
+export default function SurveyOverview({ token, surveys }) {
   const [session, loading] = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    console.log('overview session -> ', session);
-    console.log('overview loading -> ', loading);
-
     if (!session && !loading) {
       router.push('/');
-    } else {
     }
   }, [session, loading]);
+
   return (
     <div>
-      overview
-      <div>{JSON.stringify(token, null, 2)}</div>
+      <h1>overview page</h1>
+      <div>token: {JSON.stringify(token, null, 2)}</div>
+      <div>surveys.length = {surveys.length}</div>
     </div>
   );
 }
 
-export async function getServerSideProps(p) {
-  //console.log('hmmmmm', p.req);
-  const { req } = p;
+export async function getServerSideProps(ctx) {
+  const { req } = ctx;
   const secret = process.env.JWT_SECRET;
-  console.log('secret ==> ', secret);
   const token = await jwt.getToken({ req, secret });
-  console.log('JSON Web Token ==> ', token);
+
+  let surveys = [];
+  if (token) {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/surveys`,
+      {
+        headers: req.headers,
+      }
+    );
+    surveys = res.data;
+  }
 
   return {
-    props: { token },
+    props: { token, surveys },
   };
 }
