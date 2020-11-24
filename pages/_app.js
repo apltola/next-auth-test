5339e915c7b48f5285743bfe1a04a57b96ac3613
allu <aleksijaakkojuhani@gmail.com>
2020-11-24 15:25:34 +0200

    adding loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/index.css';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import { Provider } from 'next-auth/client';
-import Layout from '../components/layout';
 import { NextSeo } from 'next-seo';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { Router } from 'next/router';
+import Layout from '../components/layout';
 import {
   faBars,
   faCaretDown,
@@ -33,6 +36,11 @@ library.add(
 );
 
 function MyApp({ Component, pageProps }) {
+  Router.events.on('routeChangeStart', () => NProgress.start());
+  Router.events.on('routeChangeComplete', () => NProgress.done());
+  Router.events.on('routeChangeError', () => NProgress.done());
+  NProgress.configure({ showSpinner: false });
+
   return (
     <React.Fragment>
       <NextSeo
